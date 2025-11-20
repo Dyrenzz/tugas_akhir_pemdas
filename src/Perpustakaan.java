e5bb89848108f20bd3947ac41613cd91d683ae09
Milkkukis <fathima.amelianty@gmail.com>
2025-11-20 13:13:11 +0700

    perpus

--- src/Perpustakaan.java
+++ src/Perpustakaan.java
@@ -1,33 +1,49 @@
 public class Perpustakaan {
-    private String nama;
-    private String alamat;
+
+    String nama;
+    String alamat;
+
+    Buku[] daftarBuku = new Buku[50];
+    Member[] daftarMember = new Member[50];
+    Petugas[] daftarPetugas = new Petugas[20];
+    Peminjaman[] daftarPeminjaman = new Peminjaman[100];
+
+    int jumlahBuku = 0;
+    int jumlahMember = 0;
+    int jumlahPetugas = 0;
+    int jumlahPeminjaman = 0;
 
     public Perpustakaan(String nama, String alamat) {
         this.nama = nama;
         this.alamat = alamat;
     }
 
-    public String getNama() {
-        return nama;
+    public void tambahBuku(Buku buku) {
+        daftarBuku[jumlahBuku] = buku;
+        jumlahBuku++;
     }
 
-    public void setNama(String nama) {
-        this.nama = nama;
+    public Buku cariBuku(String judul) {
+        for (int i = 0; i < jumlahBuku; i++) {
+            if (daftarBuku[i].judul.equalsIgnoreCase(judul)) {
+                return daftarBuku[i];
+            }
+        }
+        return null;
     }
 
-    public String getAlamat() {
-        return alamat;
+    public void tampilkanDaftarBuku() {
+        for (int i = 0; i < jumlahBuku; i++) {
+            daftarBuku[i].tampilkan();
+            System.out.println("-------------------");
+        }
     }
 
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    @Override
-    public String toString() {
-        return "Perpustakaan{" +
-                "nama='" + nama + '\'' +
-                ", alamat='" + alamat + '\'' +
-                '}';
+    public Peminjaman buatPeminjaman(Buku buku, Member member, Petugas petugas) {
+        Peminjaman p = new Peminjaman((jumlahPeminjaman + 1), buku, member, petugas);
+        daftarPeminjaman[jumlahPeminjaman] = p;
+        jumlahPeminjaman++;
+        buku.dipinjam();
+        return p;
     }
 }
