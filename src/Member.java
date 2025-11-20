d243fb971d989a8b0a1a4b7fc769edbcde5f7099
COKLAT SUSU <briliant220407@gmail.com>
2025-11-20 12:40:12 +0700

    ngisi member petugas nih wkkwkwkw anjay

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,50 @@
 public class Member {
-    String nama;
-    String alamat;
+    private String nama;
+    private String alamat;
+    private String id;
+    private String noTelepon;
+
+    public Member(String id, String nama, String alamat, String noTelepon) {
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", nama='" + nama + '\'' +
+                ", alamat='" + alamat + '\'' +
+                ", noTelepon='" + noTelepon + '\'' +
+                '}';
+    }
+    
+
+
 }
--- src/Petugas.java
+++ src/Petugas.java
@@ -1,3 +1,29 @@
 public class Petugas {
-    private String idString;
+    private String id;
+    private String nama;
+
+    public Petugas(String id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+    public String getIdPetugas() {
+        return id;
+    }
+    public void setIdPetugas(String idPetugas) {
+        this.id = idPetugas;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    @Override
+    public String toString() {
+        return "Petugas{" +
+                "idPetugas='" + id + '\'' +
+                ", nama='" + nama + '\'' +
+                '}';
+    }
+
 }
