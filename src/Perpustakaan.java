75755951cfc3b43b392eb4b7a700ee37696b1390
Milkkukis <fathima.amelianty@gmail.com>
2025-11-20 11:53:02 +0700

    buat perpus

---
+++ src/Perpustakaan.java
@@ -0,0 +1,33 @@
+public class Perpustakaan {
+    private String nama;
+    private String alamat;
+
+    public Perpustakaan(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    @Override
+    public String toString() {
+        return "Perpustakaan{" +
+                "nama='" + nama + '\'' +
+                ", alamat='" + alamat + '\'' +
+                '}';
+    }
+}
